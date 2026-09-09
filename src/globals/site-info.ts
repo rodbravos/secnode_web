85fcbd765c62f063f48373da4ab66804f73aff32
Rodrigo Bravo Solis <rodbravo.s@gmail.com>
2026-09-09 00:04:10 -0300

    Mejora Navbar y agrega SEO a página

---
+++ src/globals/site-info.ts
@@ -0,0 +1,5 @@
+export const siteInfo = {
+  title: "SecNode Soluciones Tecnológicas",
+  description:
+    "Soluciones integrales de software, soporte computacional crítico y consultoría especializada en ciberseguridad y cumplimiento normativo.",
+};
